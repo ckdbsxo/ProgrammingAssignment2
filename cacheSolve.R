ccfd4975b1e7ae17bd925e26902c2e44c6772c16
ckdbsxo <dennis.yt.cha@gmail.com>
2015-03-20 22:15:44 -0700

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,12 +1,6 @@
 ## These functions cache potentially time-consuming computations, such as
 ## a very long vector, especially if has to be computed repeatedly (e.g. in a loop).
 
-## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated 
-## (and the matrix has not changed), then the cachesolve should retrieve 
-## the inverse from the cache.
-## 
-
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -23,7 +17,11 @@ makeCacheMatrix <- function(x = matrix()) {
          getinvmtx = getinvmtx)
 }
 
-
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
+## 
 ## TL; DR: Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
